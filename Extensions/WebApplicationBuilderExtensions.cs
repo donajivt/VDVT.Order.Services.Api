0bd2b83818c0a926abb6ff8aab585f482769363c
“donajivt” <vaniadonajitorres@gmail.com>
2025-08-06 18:22:34 -0600

    Se agregaron las clases de utility, de Extensions, los Servicios

---
+++ Extensions/WebApplicationBuilderExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+namespace VDVT.Order.Services.Api.Extensions
+{
+    public static class WebApplicationBuilderExtensions
+    {
+        public static WebApplicationBuilder AddAppAuthentication(this WebApplicationBuilder builder)
+        {
+            var secretKey = builder.Configuration["ApiSettings:JwtOptions:Secret"];
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey));
+
+            builder.Services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(x =>
+            {
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = key,
+                    ValidateIssuer = true,
+                    ValidIssuer = builder.Configuration["ApiSettings:JwtOptions:Issuer"],
+                    ValidAudience = builder.Configuration["ApiSettings:JwtOptions:Audience"],
+                    ValidateAudience = true
+                };
+            });
+
+            return builder;
+        }
+    }
+}
